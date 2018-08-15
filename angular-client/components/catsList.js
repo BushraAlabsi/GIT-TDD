af1045d5075e6131d0a56e3da4631e542a14f8eb
BushraAlabsi <boushra-93@live.com>
2018-08-15 10:29:55 +0300

    save a cat document in the database

--- angular-client/components/catsList.js
+++ angular-client/components/catsList.js
@@ -4,12 +4,13 @@ app.component('cats', {
 	templateUrl: '/templates/adoptCat.html'
 });
 app.controller('catsCtrl', function ($scope, $http,$window){
+//fetch all cats
 var getCats = function() {
 	// TODO: Your code here
 
 };
 getCats();
-
+// send cat to the server
 $scope.addCats=function() {
 // TODO: Your code here
 
--- database/index.js
+++ database/index.js
@@ -20,9 +20,9 @@ let catSchema = mongoose.Schema({
 let Cat = mongoose.model('Cat', catSchema);
 
 let save = (data,callback) => {
-  // TODO: Your code here
-  
-  
+  var cat = new Cat({data});
+  cat.save(callback);
+
 }
 
 module.exports.save = save;
